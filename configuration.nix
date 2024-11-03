e1095acd8082a5819d2bc11a2a228fc9cbc62df3
Guilherme Machado <machadofguilherme@proton.me>
2024-11-03 02:33:28 -0300

    :wrench: NixOS

--- configuration.nix
+++ configuration.nix
@@ -206,7 +206,7 @@ in
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix
-  system.copySystemConfiguration = true;
+  # system.copySystemConfiguration = true;
 
   # Linux
   boot.kernelPackages = pkgs.linuxPackages_latest;
