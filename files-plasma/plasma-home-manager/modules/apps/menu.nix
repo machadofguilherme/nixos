926a38fd967b1930389233b366e878787c65fc82
Guilherme Machado <machadofguilherme@proton.me>
2024-11-14 03:11:20 -0300

    :wrench: NixOS

--- files-plasma/plasma-home-manager/modules/apps/menu.nix
+++ files-plasma/plasma-home-manager/modules/apps/menu.nix
@@ -3,4 +3,29 @@
     name = "fish";
     noDisplay = true;
   };
+
+  xdg.desktopEntries."cups" = {
+    name = "Gerenciador de impressão";
+    noDisplay = true;
+  };
+
+  xdg.desktopEntries."org.freedesktop.IBus.Setup" = {
+    name = "Preferências do IBus";
+    noDisplay = true;
+  };
+
+  xdg.desktopEntries."org.kde.krdp" = {
+    name = "KRdp";
+    noDisplay = true;
+  };
+
+  xdg.desktopEntries."org.kde.kwalletmanager" = {
+    name = "KWalletManager";
+    noDisplay = true;
+  };
+
+  xdg.desktopEntries."org.kde.drkonqi.coredump.gui" = {
+    name = "Visualizador de processos com falhas";
+    noDisplay = true;
+  };
 }
