11625ab2b0649f43493843e931133f3e300b4686
Guilherme Machado <machadofguilherme@proton.me>
2024-11-21 19:12:46 -0300

    :wrench: NixOS

--- files-plasma/plasma/modules/system/packages.nix
+++ files-plasma/plasma/modules/system/packages.nix
@@ -1,11 +1,21 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  # Vivaldi customizado
+  cus_vivaldi = pkgs.vivaldi.overrideAttrs (oldAttrs: {
+    dontWrapQtApps = false;
+    dontPatchELF = true;
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+  });
+in
+{
   # Aplicações
   environment.systemPackages = with pkgs; [
     kdePackages.plasma-browser-integration
+    kdePackages.qtstyleplugin-kvantum
     vivaldi-ffmpeg-codecs
     qt6.qtwayland
     keychain
-    vivaldi
+    cus_vivaldi
     openssl
   ];
 }
