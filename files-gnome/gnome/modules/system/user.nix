9a1e0fb07bb062a423c006c5a0e8dad6b16e0726
Guilherme Machado <machadofguilherme@proton.me>
2024-11-21 00:02:04 -0300

    :wrench: NixOS

--- files-gnome/gnome/modules/system/user.nix
+++ files-gnome/gnome/modules/system/user.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   # Conta de usuário
   users.users.guilherme.isNormalUser = true;
+  users.users.guilherme.description = "Guilherme Machado";
   users.users.guilherme.extraGroups = [ 
     "wheel" 
     "audio" 
