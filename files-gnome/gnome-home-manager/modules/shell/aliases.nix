48384831a4b087fba8ecce88f13674f91a0ef4c2
Guilherme Machado <machadofguilherme@proton.me>
2024-11-21 00:29:38 -0300

    :wrench: NixOS

--- files-gnome/gnome-home-manager/modules/shell/aliases.nix
+++ files-gnome/gnome-home-manager/modules/shell/aliases.nix
@@ -14,22 +14,31 @@
 
         # Aliases para os módulos de configuração
         cfg-dev             = "sudo nano /etc/nixos/modules/misc/dev.nix";
+        cfg-nano            = "sudo nano /etc/nixos/modules/misc/config.nix";
         cfg-net             = "sudo nano /etc/nixos/modules/network/net.nix";
+        cfg-user            = "sudo nano /etc/nixos/modules/system/user.nix";
         cfg-dns             = "sudo nano /etc/nixos/modules/network/dns.nix";
         cfg-locale          = "sudo nano /etc/nixos/modules/lang/locale.nix";
-        cfg-misc            = "sudo nano /etc/nixos/modules/misc/default.nix";
         cfg-lang            = "sudo nano /etc/nixos/modules/lang/default.nix";
         cfg-boot            = "sudo nano /etc/nixos/modules/boot/default.nix";
         cfg-fonts           = "sudo nano /etc/nixos/modules/system/fonts.nix";
+        cfg-nix             = "sudo nano /etc/nixos/modules/security/nix.nix";
+        cfg-sudo            = "sudo nano /etc/nixos/modules/security/sudo.nix";
+        cfg-timezone        = "sudo nano /etc/nixos/modules/lang/timezone.nix";
         cfg-gnome           = "sudo nano /etc/nixos/modules/desktop/gnome.nix";
+        cfg-misc              = "sudo nano /etc/nixos/modules/misc/default.nix";
         cfg-shell           = "sudo nano /etc/nixos/modules/services/shell.nix";
         cfg-print           = "sudo nano /etc/nixos/modules/services/print.nix";
         cfg-audio           = "sudo nano /etc/nixos/modules/services/audio.nix";
         cfg-system          = "sudo nano /etc/nixos/modules/system/default.nix";
+        cfg-display         = "sudo nano /etc/nixos/modules/desktop/server.nix";
+        cfg-net             = "sudo nano /etc/nixos/modules/network/default.nix";
         cfg-packages        = "sudo nano /etc/nixos/modules/system/packages.nix";
         cfg-desktop         = "sudo nano /etc/nixos/modules/desktop/default.nix";
         cfg-drivers         = "sudo nano /etc/nixos/modules/desktop/drivers.nix";
         cfg-virtual         = "sudo nano /etc/nixos/modules/services/virtual.nix";
+        cfg-services        = "sudo nano /etc/nixos/modules/services/default.nix";
+        cfg-security        = "sudo nano /etc/nixos/modules/security/default.nix";
         cfg-touchpad        = "sudo nano /etc/nixos/modules/services/touchpad.nix";
         cfg-exclude-gnome   = "sudo nano /etc/nixos/modules/desktop/exclude-gnome.nix";
 
