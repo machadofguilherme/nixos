35c191f574e1903421bbca465ba45c31d5f35c2d
Guilherme Machado <machadofguilherme@proton.me>
2024-12-16 00:29:31 -0300

    :wrench: NixOS

--- files-plasma/plasma/modules/system/packages.nix
+++ files-plasma/plasma/modules/system/packages.nix
@@ -5,6 +5,9 @@
     (config._module.args.zen-browser.packages.${config._module.args.system}.specific)
     kdePackages.plasma-browser-integration
     kdePackages.qtstyleplugin-kvantum
+    kdePackages.flatpak-kcm
+    kdePackages.kcmutils
+    kdePackages.sddm-kcm
     qt6.qtwayland
     keychain
     openssl
