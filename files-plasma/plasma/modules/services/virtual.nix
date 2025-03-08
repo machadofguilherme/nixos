98b0dbc7c554a6b537c20991d0a94c0c30b3d803
Guilherme Machado <machadofguilherme@proton.me>
2025-03-08 12:59:34 -0300

    :wrench: NixOS

--- files-plasma/plasma/modules/services/virtual.nix
+++ files-plasma/plasma/modules/services/virtual.nix
@@ -4,5 +4,7 @@
 
   # Permite Flatpak
   xdg.portal.enable = true;
+  xdg.portal.config.common.default = [ "kde" ];
+  xdg.portal.extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
   services.flatpak.enable = true;
 }
