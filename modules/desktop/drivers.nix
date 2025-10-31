43c003e085aa59b452cf10ead6de27f95a4bfa52
Guilherme Machado <machadofguilherme@proton.me>
2025-10-31 08:46:54 -0300

    :wrench: NixOS

--- modules/desktop/drivers.nix
+++ modules/desktop/drivers.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }: {
   # Aceleração Gráfica
   hardware.graphics.enable = true;
-
-  # Drivers
-  hardware.graphics.extraPackages = with pkgs; [ amdvlk ];
-  hardware.graphics.extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
 }
