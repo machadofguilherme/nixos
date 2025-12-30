12ba5a3624848e195665f3509895ca412559fa54
Guilherme Machado <machadofguilherme@proton.me>
2025-12-30 01:54:54 -0300

    🔧 NixOS

--- home/modules/apps/menu.nix
+++ home/modules/apps/menu.nix
@@ -7,5 +7,6 @@
     fish = { name = "fish"; noDisplay = true; };
     cups = { name = "Gerenciador de impressão"; noDisplay = true; };
     "org.freedesktop.IBus.Setup" = { name = "Preferências do IBus"; noDisplay = true; };
+    "app.twintaillauncher.ttl" = { name = "TwintailLauncher"; exec = "false"; noDisplay = true; };
   };
 }
