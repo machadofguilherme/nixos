74ffd5b6a984661af16e5b2efdd8bbd9652d04a0
Guilherme Machado <machadofguilherme@proton.me>
2025-12-30 01:29:24 -0300

    🔧 NixOS

--- home/modules/apps/twintail.nix
+++ home/modules/apps/twintail.nix
@@ -2,11 +2,11 @@
 
 let
   twintailWrapped = pkgs.writeShellScriptBin "twintail-wrapped" ''
-    setsid ${pkgs.twintail}/bin/twintail &
+    setsid flatpak run app.twintaillauncher.ttl &
     pid=$!
     wait $pid || true
     kill -TERM -$pid 2>/dev/null || true
-  ''
+  '';
 in {
   home.packages = [
     twintailWrapped
