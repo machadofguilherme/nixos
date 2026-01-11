878c7ff0ceead3cb0f9bdeb1acaaa85f3ce01038
Guilherme Machado <machadofguilherme@proton.me>
2026-01-11 04:51:57 -0300

    🔧 NixOS

--- home/modules/apps/menu.nix
+++ home/modules/apps/menu.nix
@@ -4,10 +4,34 @@
   xdg.configFile."autostart/cloudflare-warp.desktop".text = lib.mkForce "";
 
   xdg.desktopEntries = {
-    fish = { name = "fish"; noDisplay = true; };
-    cups = { name = "Gerenciador de impressão"; noDisplay = true; };
-    "org.freedesktop.IBus.Setup" = { name = "Preferências do IBus"; noDisplay = true; };
-    "auto-cpufreq" = { name = "auto-cpufreq"; noDisplay = true; };
-    "btop++" = { name = "btop++"; noDisplay = true; };
+    btop = {
+      name = "btop++";
+      noDisplay = true;
+      exec = "";
+    };
+    
+    "auto-cpufreq-gtk" = {
+      name = "auto-cpufreq";
+      noDisplay = true;
+      exec = "";
+    };
+
+    fish = { 
+      name = "fish";
+      noDisplay = true;
+      exec = "";
+    };
+
+    cups = {
+      name = "Gerenciador de impressão";
+      noDisplay = true;
+      exec = "";
+    };
+
+    "org.freedesktop.IBus.Setup" = {
+      name = "Preferências do IBus";
+      noDisplay = true;
+      exec = "";
+    };
   };
 }
