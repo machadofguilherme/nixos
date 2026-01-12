b0650a9f274ad5c075a80f90be60d1106f5612b9
Guilherme Machado <machadofguilherme@proton.me>
2026-01-12 00:53:29 -0300

    🔧 NixOS

--- home/modules/apps/vscode.nix
+++ home/modules/apps/vscode.nix
@@ -1,92 +1,92 @@
 { pkgs, ... }:
 
+let
+  marketplace = pkgs.vscode-utils.buildVscodeMarketplaceExtension;
+in
 {
   programs.vscode = {
     enable = true;
 
-    extensions = with pkgs.vscode-extensions; [
-      # Nix (porque o editor vive dentro do sistema)
-      bbenoist.nix
-      jnoortheen.nix-ide
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # Nix
+        bbenoist.nix
+        jnoortheen.nix-ide
 
-      # Web / TypeScript / React
-      dbaeumer.vscode-eslint
-      bradlc.vscode-tailwindcss
-      dsznajder.es7-react-js-snippets
-      formulahendry.auto-rename-tag
-      mikestead.dotenv
+        # Web / TypeScript / React
+        dbaeumer.vscode-eslint
+        bradlc.vscode-tailwindcss
+        formulahendry.auto-rename-tag
+        mikestead.dotenv
 
-      # Backend / DB
-      prisma.prisma
-      mongodb.mongodb-vscode
+        # Backend / DB
+        prisma.prisma
+        mongodb.mongodb-vscode
 
-      # Qualidade de vida
-      eamodio.gitlens
-      usernamehw.errorlens
-      tamasfe.even-better-toml
-      yzhang.markdown-all-in-one
-      naumovs.color-highlight
+        # Qualidade de vida
+        eamodio.gitlens
+        usernamehw.errorlens
+        tamasfe.even-better-toml
+        yzhang.markdown-all-in-one
+        naumovs.color-highlight
 
-      # Ícones (opcional, mas funcional)
-      pkief.material-icon-theme
-    ];
-
-    userSettings = {
-      ## Arquivos
-      "files.autoSave" = "afterDelay";
-      "files.autoSaveDelay" = 1000;
-      "files.trimTrailingWhitespace" = true;
-      "files.insertFinalNewline" = true;
-
-      ## Editor
-      "editor.tabSize" = 2;
-      "editor.insertSpaces" = true;
-      "editor.formatOnSave" = true;
-      "editor.formatOnPaste" = true;
-      "editor.codeActionsOnSave" = {
-        "source.fixAll.eslint" = true;
-      };
-      "editor.wordWrap" = "on";
-      "editor.minimap.enabled" = false;
-      "editor.renderWhitespace" = "boundary";
-
-      ## Formatter
-      "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      "[javascript]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[javascriptreact]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[typescript]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[typescriptreact]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-
-      ## ESLint
-      "eslint.validate" = [
-        "javascript"
-        "javascriptreact"
-        "typescript"
-        "typescriptreact"
+        # Ícones
+        pkief.material-icon-theme
+      ]
+      ++ [
+        (marketplace {
+           mktplcRef = {
+            name = "es7-react-js-snippets";
+            publisher = "dsznajder";
+            version = "4.4.3";
+            sha256 = "sha256-QF950JhvVIathAygva3wwUOzBLjBm7HE3Sgcp7f20Pc=";
+          };
+        })
       ];
 
-      ## Tailwind
-      "tailwindCSS.emmetCompletions" = true;
-      "tailwindCSS.validate" = true;
+      userSettings = {
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 1000;
+        "files.trimTrailingWhitespace" = true;
+        "files.insertFinalNewline" = true;
 
-      ## Prisma
-      "prisma.formatOnSave" = true;
+        "editor.tabSize" = 2;
+        "editor.insertSpaces" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnPaste" = true;
+        "editor.codeActionsOnSave" = {
+          "source.fixAll.eslint" = true;
+        };
 
-      ## Git
-      "git.enableSmartCommit" = true;
+        "editor.wordWrap" = "on";
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "boundary";
 
-      ## Aparência funcional
-      "workbench.startupEditor" = "none";
-      "workbench.iconTheme" = "material-icon-theme";
-      "explorer.compactFolders" = false;
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+
+        "[javascript]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+        "[javascriptreact]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+        "[typescript]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+        "[typescriptreact]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+
+        "eslint.validate" = [
+          "javascript"
+          "javascriptreact"
+          "typescript"
+          "typescriptreact"
+        ];
+
+        "tailwindCSS.emmetCompletions" = true;
+        "tailwindCSS.validate" = true;
+
+        "prisma.formatOnSave" = true;
+
+        "git.enableSmartCommit" = true;
+
+        "workbench.startupEditor" = "none";
+        "workbench.iconTheme" = "material-icon-theme";
+        "explorer.compactFolders" = false;
+      };
     };
   };
 }
