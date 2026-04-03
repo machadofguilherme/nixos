08577e3c2dd71afe99f666cf469ee9f22dedace8
Guilherme Machado <machadofguilherme@proton.me>
2026-04-03 01:56:48 -0300

    🔧 NixOS

--- home/modules/system/gnome.nix
+++ home/modules/system/gnome.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   dconf.settings = {
     "org/gnome/shell" = {
+      disable-user-extensions = false;
       enabled-extensions = [
         "appindicatorsupport@rgcjonas.gmail.com"
         "cloudflare-warp-toggle@pavel.finkelshteyn.gmail.com"
@@ -8,9 +9,59 @@
       ];
     };
 
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Adwaita-dark";
+      icon-theme = "Adwaita";
+    };
+
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       accent-color = "red";
+      show-battery-percentage = true;
+      clock-show-weekday = true;
+      clock-show-date = true;
+      clock-show-seconds = false;
+      enable-animations = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-temperature = 3700;
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+      sleep-inactive-battery-type = "suspend";
+      sleep-inactive-battery-timeout = 900;
+    };
+
+    "org/gnome/desktop/sound" = {
+      allow-volume-above-100-percent = true;
+    };
+
+    "org/gnome/desktop/privacy" = {
+      remove-old-temp-files = true;
+      remove-old-trash-files = true;
+      old-files-age = 14;
+      remember-recent-files = false;
+      report-technical-problems = false;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      natural-scroll = true;
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = [ ];
+      switch-applications-backward = [ ];
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
     };
   };
 
