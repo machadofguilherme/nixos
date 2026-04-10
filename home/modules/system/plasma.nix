2b0bb16c42f5a8d72b85f4b874c26aedf3ff727b
Guilherme Machado <machadofguilherme@proton.me>
2026-04-10 11:26:55 -0300

    🔧 NixOS

--- home/modules/system/plasma.nix
+++ home/modules/system/plasma.nix
@@ -1,20 +1,30 @@
 { config, pkgs, ... }: {
-
   programs.plasma = {
     enable = true;
+
     workspace = {
       lookAndFeel = "org.kde.breezedark.desktop";
-      theme = "breeze-dark";
       colorScheme = "BreezeDark";
       iconTheme = "breeze-dark";
+      theme = "breeze-dark";
     };
 
     kwin.effects = {
       shakeCursor.enable = false;
     };
 
-    configFile."kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
-    configFile."dolphinrc"."General"."ViewPropsGroupView" = "Empty";
-    configFile."dolphinrc"."Main View"."ShowHiddenFiles" = true;
+    krunner = {
+      activateWhenTypingOnDesktop = true;
+      shortcuts.launch = "Alt+Space";
+      historyBehavior = "disabled";
+      position = "center";
+    };
+
+    configFile = {
+      "kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
+
+      "dolphinrc"."General"."ViewPropsGroupView" = "Empty";
+      "dolphinrc"."Main View"."ShowHiddenFiles" = true;
+    };
   };
 }
