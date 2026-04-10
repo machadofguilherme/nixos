762a6391d2e95b09c48d3fb5068ef5c398889a10
Guilherme Machado <machadofguilherme@proton.me>
2026-04-10 12:23:38 -0300

    🔧 NixOS

--- home/modules/system/plasma.nix
+++ home/modules/system/plasma.nix
@@ -1,30 +1,47 @@
 { config, pkgs, ... }: {
-  programs.plasma = {
-    enable = true;
+  programs = {
+    plasma = {
+      enable = true;
 
-    workspace = {
-      lookAndFeel = "org.kde.breezedark.desktop";
-      colorScheme = "BreezeDark";
-      iconTheme = "breeze-dark";
-      theme = "breeze-dark";
+      workspace = {
+        lookAndFeel = "org.kde.breezedark.desktop";
+        colorScheme = "BreezeDark";
+        iconTheme = "breeze-dark";
+        theme = "breeze-dark";
+      };
+
+      kwin.effects = {
+        shakeCursor.enable = false;
+      };
+
+      krunner = {
+        activateWhenTypingOnDesktop = true;
+        shortcuts.launch = "Alt+Space";
+        historyBehavior = "disabled";
+        position = "center";
+      };
+
+      configFile = {
+        "kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
+
+        "dolphinrc"."General"."ViewPropsGroupView" = "Empty";
+        "dolphinrc"."Main View"."ShowHiddenFiles" = true;
+      };
     };
 
-    kwin.effects = {
-      shakeCursor.enable = false;
-    };
+    kate = {
+      editor = {
+        tabWidth = 2;
+        brackets = {
+          automaticallyAddClosing = true;
+          characters = "<>(){}[]'\"`*_~";
+        };
 
-    krunner = {
-      activateWhenTypingOnDesktop = true;
-      shortcuts.launch = "Alt+Space";
-      historyBehavior = "disabled";
-      position = "center";
-    };
-
-    configFile = {
-      "kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
-
-      "dolphinrc"."General"."ViewPropsGroupView" = "Empty";
-      "dolphinrc"."Main View"."ShowHiddenFiles" = true;
+        indent = {
+          autodetect = true;
+          backspaceDecreaseIndent = true;
+        };
+      };
     };
   };
 }
