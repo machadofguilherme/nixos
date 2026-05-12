c3b93eefacfb1346f75c2d89eabaf554f74a6c30
Guilherme Machado <machadofguilherme@proton.me>
2026-05-12 18:42:33 -0300

    🔧 NixOS

--- modules/network/net.nix
+++ modules/network/net.nix
@@ -3,21 +3,25 @@
     hostName = "NixOS";
     networkmanager = {
       enable = true;
+      wifi.powersave = false;
       dns = "systemd-resolved";
+      connectionConfig."ipv4.ignore-auto-dns" = true;
     };
-    
+
     resolvconf.enable = false;
     enableIPv6 = false;
   };
 
   services.resolved = {
     enable = true;
-    settings.Resolve = {
-      DNSSEC = "false";
-      FallbackDNS = [
-        "1.1.1.2"
-        "9.9.9.9"
-      ];
+    settings = {
+      Resolve = {
+        DNSSEC = "false";
+        DNS = "1.1.1.2 1.0.0.2";
+        FallbackDNS = "9.9.9.9";
+        Domains = "~.";
+        DNSStubListenerExtra = "127.0.0.53";
+      };
     };
   };
 
