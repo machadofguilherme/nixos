c1c77b7b3abc31163ea2e97eb06196762d4452c3
Guilherme Machado <machadofguilherme@proton.me>
2026-05-12 22:34:02 -0300

    🔧 NixOS

--- home/modules/apps/vscode.nix
+++ home/modules/apps/vscode.nix
@@ -6,44 +6,70 @@ in
 {
   programs.vscode = {
     enable = true;
-
-    # evita o VSCode tentar escrever/extensões dinamicamente
     mutableExtensionsDir = false;
 
     profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        # Idioma
-        ms-ceintl.vscode-language-pack-pt-br
+      userSettings = {
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 500;
+        "files.trimTrailingWhitespace" = true;
+        "files.insertFinalNewline" = true;
+        "editor.tabSize" = 2;
+        "editor.insertSpaces" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnPaste" = true;
+        "editor.codeActionsOnSave" = {
+          "source.fixAll.eslint" = "explicit";
+        };
+        "editor.wordWrap" = "on";
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "boundary";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[javascriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "eslint.validate" = [
+          "javascript"
+          "javascriptreact"
+          "typescript"
+          "typescriptreact"
+        ];
+        "tailwindCSS.emmetCompletions" = true;
+        "tailwindCSS.validate" = true;
+        "prisma.formatOnSave" = true;
+        "git.enableSmartCommit" = true;
+        "workbench.startupEditor" = "none";
+        "workbench.iconTheme" = "material-icon-theme";
+        "settingsSync.enabled" = false;
+        "update.mode" = "none";
+        "extensions.autoUpdate" = false;
+        "extensions.autoCheckUpdates" = false;
+        "telemetry.telemetryLevel" = "off";
+        "locale" = "pt-BR";
+        "workbench.colorCustomizations" = null;
+        "js/ts.updateImportsOnFileMove.enabled" = "always";
+      };
 
-        # Nix
+      extensions = with pkgs.vscode-extensions; [
+        ms-ceintl.vscode-language-pack-pt-br
         bbenoist.nix
         jnoortheen.nix-ide
-
-        # Web / TypeScript / React
         dbaeumer.vscode-eslint
         bradlc.vscode-tailwindcss
         formulahendry.auto-rename-tag
         mikestead.dotenv
-
-        # Rust
         rust-lang.rust-analyzer
-
-        # Backend / DB
         prisma.prisma
         mongodb.mongodb-vscode
-
-        # Qualidade de vida
         eamodio.gitlens
         usernamehw.errorlens
         tamasfe.even-better-toml
         yzhang.markdown-all-in-one
         naumovs.color-highlight
         esbenp.prettier-vscode
-
-        # Ícones
         pkief.material-icon-theme
-      ]
-      ++ [
+      ] ++ [
         (marketplace {
           mktplcRef = {
             name = "es7-react-js-snippets";
@@ -55,10 +81,4 @@ in
       ];
     };
   };
-
-  home.file.".config/Code/User/settings.json".source =
-    lib.mkForce (
-      config.lib.file.mkOutOfStoreSymlink
-        "/etc/nixos/vscode/settings.json"
-    );
 }
